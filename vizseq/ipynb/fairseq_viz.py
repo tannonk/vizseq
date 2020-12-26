b9ac297a178685f4b25e205fc88520e7aa599e82
Tannon Kew <kew@vigrid.cli>
2020-12-26 04:13:46 +0100

    added _get_data_with_ids to include ids in return value for _get_data

--- vizseq/ipynb/fairseq_viz.py
+++ vizseq/ipynb/fairseq_viz.py
@@ -15,7 +15,6 @@ from vizseq.ipynb.core import (view_examples as _view_examples,
                                view_scores as _view_scores)
 from vizseq._view import DEFAULT_PAGE_SIZE, DEFAULT_PAGE_NO, VizSeqSortingType
 
-
 def _get_data(log_path_or_paths: Union[str, List[str]], nbest: int = 1):
     if isinstance(log_path_or_paths, str):
         log_path_or_paths = [log_path_or_paths]
@@ -64,6 +63,59 @@ def _get_data(log_path_or_paths: Union[str, List[str]], nbest: int = 1):
             hypo[name] = [cur_hypo[i][:nbest] for i in cur_ids]
 
     return {'0': src}, {'0': ref}, hypo
+
+def _get_data_with_ids(log_path_or_paths: Union[str, List[str]], nbest: int = 1):
+    """
+    same as above _get_data function but returns ids from
+    fairseq output file as well.
+    """
+    if isinstance(log_path_or_paths, str):
+        log_path_or_paths = [log_path_or_paths]
+    ids, src, ref, hypo = None, None, None, {}
+    names = Counter()
+    for k, log_path in enumerate(log_path_or_paths):
+        assert op.isfile(log_path)
+        cur_src, cur_ref, cur_hypo = {}, {}, {}
+        with open(log_path) as f:
+            for l in f:
+                line = l.strip()
+                if line.startswith('H-'):
+                    _id, _, sent = line.split('\t', 2)
+                    # cur_hypo[_id[2:]] = sent
+                    # collect multiple hypotheses for each ID (nbest > 1)
+                    if not _id[2:] in cur_hypo:
+                        cur_hypo[_id[2:]] = [sent]
+                    else:
+                        cur_hypo[_id[2:]].append(sent)
+                elif line.startswith('T-'):
+                    _id, sent = line.split('\t', 1)
+                    cur_ref[_id[2:]] = sent
+                elif line.startswith('S-'):
+                    _id, sent = line.split('\t', 1)
+                    cur_src[_id[2:]] = sent
+        cur_ids = sorted(cur_src.keys())
+        assert set(cur_ids) == set(cur_ref.keys()) == set(cur_hypo.keys())
+        cur_src = [cur_src[i] for i in cur_ids]
+        cur_ref = [cur_ref[i] for i in cur_ids]
+        if k == 0:
+            ids, src, ref = cur_ids, cur_src, cur_ref
+        else:
+            assert set(ids) == set(cur_ids) and set(src) == set(cur_src)
+            assert set(ref) == set(cur_ref)
+        name = op.splitext(op.basename(log_path))[0]
+        names.update([name])
+        if names[name] > 1:
+            name += f'.{names[name]}'
+        # hypo[name] = [cur_hypo[i] for i in cur_ids]
+        if nbest == 1:
+            # ensure that hypo = {'name': ['hyp1', 'hyp2', etc]}
+            hypo[name] = [cur_hypo[i][:nbest][0] for i in cur_ids]
+        else:
+            # hypo will be {'name': [['hyp1.1', 'hyp1.2', 'hyp1.3', ...], ['hyp2.1', 'hyp2.2', 'hyp2.3', ...], [etc]]}
+            # other functions will need to be extended for compatibility
+            hypo[name] = [cur_hypo[i][:nbest] for i in cur_ids]
+
+    return {'0': src}, {'0': ref}, hypo, ids
 
 
 # TODO: visualize alignment by attention
